efe1f8ae66615637728c0328cdd1752f1181a073
NadinLo <nadin.loewe@posteo.de>
2020-04-07 16:11:37 +0200

    realized Scanner input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,44 @@
 package com.company;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Connection conn = null;
+        Scanner myScanner = new Scanner(System.in);
         try {
             String url = "jdbc:mysql://localhost:3306/notizbuch?user=root";
             conn = DriverManager.getConnection(url);
             System.out.println("got it!");
+            String query = "";
+
+//Select in my calendar
             System.out.println("my calendar");
-            String query = "SELECT TIMESTAMP(`datum`,`zeit`) AS Wann, " +
-                    "DATE_ADD(TIMESTAMP(`datum`,`zeit`), INTERVAL `zeitspanne` HOUR_SECOND) AS bis, " +
-                    "`event`, `kommentar` FROM `kalender`";
+            String input1 = "";
+            String input2 = "";
+
+            System.out.println("it'll print: date (start and end) of an event with a comment. You can choose a specific time frame" +
+                    "\n enter a date ('yyyy-mm-dd') or time frame (1.input: > 'yyyy-mm-dd' 2.input: < 'yyyy-mm-dd')!");
+            input1 = myScanner.nextLine();
+//time frame:
+            if (input1.startsWith(">")) {
+                input1 = "WHERE datum " + input1;
+                input2 = myScanner.nextLine();
+                input2 = "AND datum " + input2;
+                query = "SELECT TIMESTAMP(`datum`,`zeit`) AS Wann, " +
+                        "DATE_ADD(TIMESTAMP(`datum`,`zeit`), INTERVAL `zeitspanne` HOUR_SECOND) AS bis, " +
+                        "`event`, `kommentar` FROM `kalender`" + input1 + input2;
+            }
+//one specific date:
+            else {
+                input1 = "WHERE datum =" + input1;
+                query = "SELECT TIMESTAMP(`datum`,`zeit`) AS Wann, " +
+                        "DATE_ADD(TIMESTAMP(`datum`,`zeit`), INTERVAL `zeitspanne` HOUR_SECOND) AS bis, " +
+                        "`event`, `kommentar` FROM `kalender`" + input1;
+            }
+//output:
             try (Statement stmt = conn.createStatement()) {
                 ResultSet rs = stmt.executeQuery(query);
                 while (rs.next()) {
@@ -29,6 +54,8 @@ public class Main {
                 throw new Error("Problem", e);
             }
 
+
+//select in my journal:
             System.out.println("my journals");
             query = "SELECT * FROM `eintrag`";
             try (Statement stmt = conn.createStatement()) {
@@ -42,11 +69,13 @@ public class Main {
             } catch (SQLException e) {
                 throw new Error("Problem", e);
             }
-
+//insert to my journal:
             System.out.println("insert to journal");
-            String command = "INSERT INTO `eintrag`(`titel`, `log`) VALUES ('Dienstag Nachmittag', 'Löwenzahnhonig ist fertig " +
-                    "(wahrscheinlich misslungen), eir haben Mittag gegessen (Reste von gestern Abend), Gemüsekiste ist " +
-                    "gekommen! Ich freue mich über Rhabarber, code nun weiter und komme gut voran.')";
+            System.out.println("please enter the title of your entry");
+            String title = myScanner.nextLine();
+            System.out.println("now enter your log text");
+            String log = myScanner.nextLine();
+            String command = "INSERT INTO `eintrag`(`titel`, `log`) VALUES ('"+title+"', '"+log+"')";
             try (Statement stmt = conn.createStatement()) {
                 stmt.executeUpdate(command);
             } catch (SQLException e) {
@@ -63,6 +92,5 @@ public class Main {
                 System.out.println(e.getMessage());
             }
         }
-
     }
 }
